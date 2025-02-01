9fb07197913776d8524f2429dcf84c940d4e2dfd
DustIn <117292644+DustInTheWoods@users.noreply.github.com>
2025-02-01 17:31:40 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/CLionProjects/AdvancedCacheManager
-# Build directory: /root/CLionProjects/AdvancedCacheManager/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[AdvancedCacheManagerTests]=] "/root/CLionProjects/AdvancedCacheManager/cmake-build-debug/AdvancedCacheManagerTests")
-set_tests_properties([=[AdvancedCacheManagerTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/root/CLionProjects/AdvancedCacheManager/CMakeLists.txt;53;add_test;/root/CLionProjects/AdvancedCacheManager/CMakeLists.txt;0;")
